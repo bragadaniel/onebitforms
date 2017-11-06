e55681e766fb74ae7b4a15fba4f17d761df11e00
Daniel Oliveira <daniel_oliveira0@hotmail.com>
2017-11-06 19:21:15 -0200

    Capture errors bad request and not found

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,12 @@
 module Api::V1
   
   class ApiController < ApplicationController
-    
+    rescue_from(ActiveRecord::RecordNotFount) do ||
+      render(json: {message: 'Not Found'}, status: :not_found)
+    end
+    rescue_from(ActiveRecord::ParameterMissing) do |parameter_missing_exception|
+      render(json: {message: "Required parameter missing: #{parameter_missing_exception.param}"}, status: :bad_request)
+    end
   end
   
 end
