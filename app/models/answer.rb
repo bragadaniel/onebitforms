b92ddd18abde92c14add494b36031a64e562e53a
Daniel Oliveira <daniel_oliveira0@hotmail.com>
2017-11-06 19:13:29 -0200

    Create friendly-id, create models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,17 @@
 class Answer < ApplicationRecord
-  belongs_to :user
   belongs_to :form
+  has_many :question_answers, dependent: :destroy
+  accepts_nested_attributes_for :question_answers
+  validates :form, presence: true
+
+  def self.create_with_questions form, question_answers
+    answer = nill
+    ActiveRecord::Base.transaction do
+      answer = Answer.create(form: form)
+      question_answers.each do |qa|
+        answer.question_answers.create(qa.permit(:question_id, :value))
+      end
+    end
+    answer
+  end
 end
